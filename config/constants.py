205a4b3b9798cdac702a1254d6d11ca95f3dda20
juanvieira <juanvieira1@yahoo.com>
2025-04-03 14:34:18 -0300

    feat: add datetime to get it with timezone

--- config/constants.py
+++ config/constants.py
@@ -2,6 +2,8 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
+import datetime
+import pytz
 from dotenv import load_dotenv
 
 FOUND_DOTENV = load_dotenv()
@@ -26,6 +28,15 @@ class EnvVars:
             raise ValueError(f'This environment variable "{env_var_name}" was not found. '
                              'Verify your .env.')
         return value
+
+class DatetimeFormats:
+    @staticmethod
+    def get_datetime() -> str:
+        local_time_zone = pytz.timezone("America/Sao_Paulo")
+        with_timezone = datetime.datetime.now(local_time_zone)
+        timestamp_postgre = with_timezone.strftime('%Y-%m-%d %H:%M:%S%z')
+
+        return timestamp_postgre
         
 if __name__ == "__main__":
     EnvVars()
