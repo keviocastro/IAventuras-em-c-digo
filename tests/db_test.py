32022a60bfe0c260996c89440be7a3a324dd9a4e
juanvieira <juanvieira1@yahoo.com>
2025-04-05 10:46:45 -0300

    test: add unit test for get datetime methods

--- tests/db_test.py
+++ tests/db_test.py
@@ -4,7 +4,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 
 import pytest
 from utils.db.crud import PostgreSQLDatabase
-from config.constants import EnvVars
+from config.project_constants import EnvVars
+from config.project_constants import DatetimeFormats
 
 env = EnvVars()
 db_password = env.get_var("DB_PASSWORD")
@@ -22,3 +23,13 @@ def connect_db():
 
 def test_db_connection():
     assert connect_db() == True
+
+def get_correct_datetimes():
+    return DatetimeFormats.get_datetime()
+
+def get_correct_datetimes_6m():
+    return DatetimeFormats.get_datetime_plus_6_months()
+
+def test_get_correct_datetimes():
+    assert isinstance(get_correct_datetimes(), str)
+    assert isinstance(get_correct_datetimes_6m(), str)
