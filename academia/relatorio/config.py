3bcb8f2bbd268e4da8ac76bd96e692361ef80f43
Andre Luiz <92170539+anderTron1@users.noreply.github.com>
2025-04-25 00:11:41 -0300

    Delete academia/relatorio/config.py

--- academia/relatorio/config.py
+++
@@ -1,6 +0,0 @@
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'andreluizpires1507@gmail.com'
-MAIL_PASSWORD = '@Ndertron123'
-MAIL_DEFAULT_SENDER = 'andreluizpires1507@gmail.com'
